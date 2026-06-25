77203e94f5cd0f4f92d4f740640bf55fec90651e
MUH. ABDILLAH MUKHLIS <muhabdillahmukhlis@apps.ipb.ac.id>
2026-06-25 18:56:21 +0700

    Selesai membuat struktur dasar HUD Tugas 2.

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,18 +1,17 @@
 extends CanvasLayer
 
 func _ready():
-	# Memastikan kursor mouse muncul dan bisa diklik di Main Menu
+	# Memastikan kursor mouse muncul
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
-	# Menghubungkan tombol secara otomatis lewat kode
+	# Menghubungkan tombol langsung lewat kode (Pengganti klik panel kanan)
 	$Control/StartButton.connect("pressed", self, "_on_StartButton_pressed")
 	$Control/ExitButton.connect("pressed", self, "_on_ExitButton_pressed")
-
 # Fungsi ini akan berjalan saat tombol Start Game diklik
 func _on_StartButton_pressed():
-	# TUGAS: Ganti "res://Gameplay.tscn" di bawah ini dengan nama file scene memanjatmu!
-	get_tree().change_scene("res://Gameplay.tscn")
+	# Ganti "res://Lobbly.tscn" di bawah ini dengan nama file scene gameplay utamamu!
+	get_tree().change_scene("res://Lobbly.tscn")
 
 # Fungsi ini akan berjalan saat tombol Exit diklik
 func _on_ExitButton_pressed():
-	get_tree().quit() # Mengeluarkan pemain dari game
+	get_tree().quit() # Perintah untuk menutup/keluar dari game
